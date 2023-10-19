27e5f9cd847cc3171a9b793b0579461086112943
CrimsonAsh07 <ashwinskap65@gmail.com>
2023-10-19 17:49:04 +0530

    Q2 Update

--- Assignment 1/q2/2a.cpp
+++ Assignment 1/q2/2a.cpp
@@ -3,19 +3,18 @@
 #include <openssl/hmac.h>
 #include <openssl/md5.h>
 
+//g++ -o 2a 2a.cpp -lssl -lcrypto
+
 int main() {
-    // Message and secret key
     const char* message = "Hello, World!";
     const char* secret_key = "MySecretKey";
 
-    // Calculate HMAC-MD5
     unsigned char digest[MD5_DIGEST_LENGTH];
     HMAC_CTX* ctx = HMAC_CTX_new();
     HMAC_Init_ex(ctx, secret_key, strlen(secret_key), EVP_md5(), NULL);
     HMAC_Update(ctx, reinterpret_cast<const unsigned char*>(message), strlen(message));
     HMAC_Final(ctx, digest, NULL);
 
-    // Print the HMAC result
     std::cout << "HMAC-MD5: ";
     for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
         printf("%02x", digest[i]);
--- Assignment 1/q2/2b.cpp
+++ Assignment 1/q2/2b.cpp
@@ -4,18 +4,15 @@
 #include <openssl/sha.h>
 
 int main() {
-    // Message and secret key
     const char* message = "Hello, World!";
     const char* secret_key = "MySecretKey";
 
-    // Calculate HMAC-SHA256
     unsigned char digest[SHA256_DIGEST_LENGTH];
     HMAC_CTX* ctx = HMAC_CTX_new();
     HMAC_Init_ex(ctx, secret_key, strlen(secret_key), EVP_sha256(), NULL);
     HMAC_Update(ctx, reinterpret_cast<const unsigned char*>(message), strlen(message));
     HMAC_Final(ctx, digest, NULL);
 
-    // Print the HMAC result
     std::cout << "HMAC-SHA256: ";
     for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
         printf("%02x", digest[i]);
